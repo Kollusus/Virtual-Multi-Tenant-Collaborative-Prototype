ee8ca450a3e69195e1f36363296dc1fd17d5e852
Kollusus <kissasza72@email.com>
2026-02-16 16:12:31 +0700

    Configure ESLint, Prettier, and Next.js performance setting

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [process.env.NEXTAUTH_URL ?? ""],
+  // basePath: "/testnextjs",
+  // assetPrefix: "/testnextjs",
+  experimental: {
+    optimizePackageImports: [
+      "@mantine/core",
+      "@mantine/dates",
+      "@mantine/form",
+      "@mantine/hooks",
+      "@mantine/utils",
+      "@tabler/icons-react",
+    ],
+  },
+  devIndicators: false,
+  productionBrowserSourceMaps: true,
 };
 
 export default nextConfig;
